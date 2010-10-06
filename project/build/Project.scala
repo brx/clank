2851fc7e67f1c9b85971279746632f54db402cb7
Andreas Scholta <andreas.scholta@gmail.com>
2010-10-06 19:13:08 +0200

    Change Proguard plugin configuration to automatically include scala-library.jar

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -5,6 +5,8 @@ class Project(info: ProjectInfo) extends DefaultProject(info) with ProguardProje
   val scalatest = "org.scalatest" % "scalatest" % "1.2"
   val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.0" % "1.7"
 
+  override def proguardInJars = super.proguardInJars +++ buildLibraryJar
+
   override val proguardOptions =
     List("-keep class com.clank.Clank { public static void main(java.lang.String[]); }",
          "-dontnote")
